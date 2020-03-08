cfbb8323b9f5f0b3872c51dedb7ef6a7b960e8a0
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-08 15:43:19 -0400

    got map of breweries as accordion

--- src/components/AreaHome/AreaHome.js
+++ src/components/AreaHome/AreaHome.js
@@ -42,13 +42,41 @@ class AreaHome extends Component {
       let listofThem = this.state.currentObj;
       let list = listofThem.map(item => {
         return (
-          <div>
-            <p>
-              {/* <Link to={"/infopage/" + item.parkCode}>{item.fullName}</Link>:{" "}
-                  {item.fullName} */}
-              {item.name}
-            </p>
+          //   <div>
+          //     <p>
+          //       <Link to={"/infopage/" + item.parkCode}>{item.fullName}</Link>:{" "}
+          //           {item.fullName}
+          //       {item.name}
+          //     </p>
+          //   </div>
+          <div className="card">
+            <div className="card-header" role="tab" id="headingOne">
+              <h5 className="mb-0">
+                <a
+                  data-toggle="collapse"
+                  href="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  {item.name}
+                </a>
+              </h5>
+            </div>
+
+            <div
+              id="collapseOne"
+              className="collapse show"
+              role="tabpanel"
+              aria-labelledby="headingOne"
+            >
+              <div className="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid.
+              </div>
+            </div>
           </div>
+
+          //   </div>
         );
       });
       return <div id="container">{list}</div>;
